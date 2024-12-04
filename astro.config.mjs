1b9df90b85878c7f72ef6c05392b040c5bff7e5c
Felipe <felipe@gooseinsurance.com>
2024-12-04 14:49:03 -0800

    separate builds - test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.SITE_NAME || 'site1'}` });
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.BASE_URL,
+  //outDir: `dist-${process.env.SITE_NAME}`,
+  output: `static-${process.env.SITE_NAME}`,
+  adapter: vercel({ edgeMiddleware: true, })
+});
