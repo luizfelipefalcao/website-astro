356df2d1d4681c54fccd461f3cda0a221dab3f2d
Felipe <felipe@gooseinsurance.com>
2024-12-04 15:01:08 -0800

    build SB website test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/static";
 import dotenv from 'dotenv';
 
 dotenv.config({ path: `.env.${process.env.SITE_NAME || 'site1'}` });
@@ -6,7 +7,7 @@ dotenv.config({ path: `.env.${process.env.SITE_NAME || 'site1'}` });
 // https://astro.build/config
 export default defineConfig({
   site: process.env.BASE_URL,
-  //outDir: `dist-${process.env.SITE_NAME}`,
-  output: `static-${process.env.SITE_NAME}`,
-  adapter: vercel({ edgeMiddleware: true, })
+  outDir: `dist-${process.env.SITE_NAME}`,
+  //output: "static",
+  //adapter: vercel({ edgeMiddleware: true, })
 });
