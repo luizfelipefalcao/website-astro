bc956bc77113d03533eaa0067275f82d443973ef
Felipe <felipe@gooseinsurance.com>
2024-12-05 20:01:14 -0800

    added blablabla to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 //import vercel from "@astrojs/vercel/static";
 import dotenv from 'dotenv';
 
-dotenv.config({ path: `.env.${process.env.SITE_NAME || 'site1'}` });
+dotenv.config({ path: `.env.${process.env.SITE_NAME || 'blablabla'}` });
 
 // https://astro.build/config
 export default defineConfig({
