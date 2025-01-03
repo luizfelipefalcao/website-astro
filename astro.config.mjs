c023847661db28a986c5bbf3d4725383bdde2e82
Felipe <felipe@gooseinsurance.com>
2025-01-02 16:19:35 -0800

    added back welcome.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,33 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
 import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import { loadEnv } from 'vite';
 
 import { redirectsGoose } from "./redirects/goose";
 import { redirectsSmartbunny } from "./redirects/smartbunny";
 
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 const redirects = process.env.BRAND.includes('goose') ? redirectsGoose : redirectsSmartbunny;
 const GOOSE_WEBSITE = 'https://www.gooseinsurance.com';
 const SMARTBUNNY_WEBSITE = 'https://www.smartbunny.com/';
 const siteName = process.env.BRAND.includes('goose') ? GOOSE_WEBSITE : SMARTBUNNY_WEBSITE;
 
-// https://astro.build/config
 export default defineConfig({
   site: siteName,
   redirects,
   output: "static",
   adapter: vercel(),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page'
+    },
+    apiOptions: {
+      region: 'us'
+    }
+  })]
 });
